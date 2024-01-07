7643b3d557f085754d8cb6c142e7676f8cc71678
serdeam002 <thanawanphansa2020@gmail.com>
2024-01-08 03:53:37 +0700

    add website

--- server.py
+++ server.py
@@ -160,10 +160,6 @@ def check_computer_usage_server():
 
 ###################showdatainwebsite######################
 
-app.config['JWT_SECRET_KEY'] = secrets.token_urlsafe(32)
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
-jwt = JWTManager(app)
-
 # เพิ่มข้อมูล
 @app.route('/api/adddata', methods=['POST'])
 @jwt_required()
